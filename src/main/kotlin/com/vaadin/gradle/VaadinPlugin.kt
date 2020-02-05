5cab5ab229a34a7a13c61ec6da1fe6c78fd06fb2
Martin Vysny <mavi@vaadin.com>
2020-02-05 10:59:52 +0200

    VaadinPlugin rewritten to Kotlin

---
+++ src/main/kotlin/com/vaadin/gradle/VaadinPlugin.kt
@@ -0,0 +1,28 @@
+package com.vaadin.gradle
+
+import com.moowork.gradle.node.NodePlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+
+/**
+ * @author mavi
+ */
+class VaadinPlugin : Plugin<Project> {
+    override fun apply(project: Project) {
+        // we need Java Plugin conventions so that we can ensure the order of tasks
+        project.pluginManager.apply(JavaPlugin::class.java)
+        project.pluginManager.apply(NodePlugin::class.java)
+        project.extensions.create("vaadin", VaadinFlowPluginExtension::class.java, project)
+
+        project.tasks.apply {
+            findByPath("clean")!!.doLast {
+                project.delete("${project.projectDir}/target")
+            }
+            register("vaadinClean", VaadinCleanTask::class.java)
+            register("vaadinPrepareFrontend", VaadinPrepareFrontendTask::class.java)
+            register("vaadinBuildFrontend", VaadinBuildFrontendTask::class.java)
+            register("vaadinPrepareNode", VaadinPrepareNodeTask::class.java)
+        }
+    }
+}
