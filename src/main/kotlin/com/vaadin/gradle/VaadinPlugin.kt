965f353ff71605c20c478153fac933a25ec32d30
Martin Vysny <mavi@vaadin.com>
2020-02-26 11:30:16 +0200

    remove unused imports

--- src/main/kotlin/com/vaadin/gradle/VaadinPlugin.kt
+++ src/main/kotlin/com/vaadin/gradle/VaadinPlugin.kt
@@ -20,8 +20,6 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.SourceSetContainer
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.tasks.bundling.War
 
 /**
  * @author mavi
