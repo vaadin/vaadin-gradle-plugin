54bfa07f0af05dc308cda5457b680370a2a4c2b4
Martin Vysny <mavi@vaadin.com>
2021-03-17 11:04:28 +0200

    Reimplement Tasks to use BuildFrontendUtil

--- src/main/kotlin/com/vaadin/gradle/VaadinBuildFrontendTask.kt
+++ src/main/kotlin/com/vaadin/gradle/VaadinBuildFrontendTask.kt
@@ -15,17 +15,12 @@
  */
 package com.vaadin.gradle
 
+import com.vaadin.flow.plugin.base.BuildFrontendUtil
 import com.vaadin.flow.server.Constants
-import com.vaadin.flow.server.InitParameters
-import com.vaadin.flow.server.frontend.FrontendTools
 import com.vaadin.flow.server.frontend.FrontendUtils
-import com.vaadin.flow.server.frontend.FrontendUtils.DEAULT_FLOW_RESOURCES_FOLDER
-import elemental.json.JsonObject
-import elemental.json.impl.JsonUtil
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.bundling.Jar
-import java.io.File
 
 /**
  * Task that builds the frontend bundle.
@@ -66,99 +61,18 @@ public open class VaadinBuildFrontendTask : DefaultTask() {
 
     @TaskAction
     public fun vaadinBuildFrontend() {
-        val extension: VaadinFlowPluginExtension = VaadinFlowPluginExtension.get(project)
-        val tokenFile = File(extension.webpackOutputDirectory, FrontendUtils.TOKEN_FILE)
+        val extension: VaadinFlowPluginExtension =
+            VaadinFlowPluginExtension.get(project)
         logger.info("Running the vaadinBuildFrontend task with effective configuration $extension")
+        val adapter = GradlePluginAdapter(project)
+        BuildFrontendUtil.updateBuildFile(adapter)
 
-        updateBuildFile(tokenFile)
+        BuildFrontendUtil.runNodeUpdater(adapter)
 
-        runNodeUpdater(extension, tokenFile)
-
-        if (extension.generateBundle) {
-            runWebpack(extension)
+        if (adapter.generateBundle()) {
+            BuildFrontendUtil.runWebpack(adapter)
         } else {
             logger.info("Not running webpack since generateBundle is false")
         }
-    }
-
-    private fun runWebpack(extension: VaadinFlowPluginExtension) {
-        val webpackCommand = "webpack/bin/webpack.js"
-        val webpackExecutable = File(extension.npmFolder, FrontendUtils.NODE_MODULES + webpackCommand)
-        check(webpackExecutable.isFile) {
-            "Unable to locate webpack executable by path '${webpackExecutable.absolutePath}'. Double check that the plugin is executed correctly"
-        }
-        val tools: FrontendTools = extension.createFrontendTools()
-        val nodePath: String = if (extension.requireHomeNodeExec) {
-            tools.forceAlternativeNodeExecutable()
-        } else {
-            tools.nodeExecutable
-        }
-
-        logger.info("Running webpack")
-        exec(project.logger, project.projectDir, nodePath, webpackExecutable.absolutePath)
-    }
-
-    private fun runNodeUpdater(extension: VaadinFlowPluginExtension, tokenFile: File) {
-        val jarFiles: Set<File> = project.configurations.getByName("runtimeClasspath")
-                .resolve()
-                .filter { it.name.endsWith(".jar") }
-                .toSet()
-
-        logger.info("runNodeUpdater: npmFolder=${extension.npmFolder}, generatedPath=${extension.generatedFolder}, frontendDirectory=${extension.frontendDirectory}")
-        logger.info("runNodeUpdater: runNpmInstall=${extension.runNpmInstall}, enablePackagesUpdate=true, useByteCodeScranner=${extension.optimizeBundle}")
-        logger.info("runNodeUpdater: copyResources=${jarFiles.toPrettyFormat()}")
-        logger.info("runNodeUpdater: copyLocalResources=${extension.frontendResourcesDirectory}")
-        logger.info("runNodeUpdater: enableImportsUpdate=true, embeddableWebComponents=${extension.generateEmbeddableWebComponents}, tokenFile=${tokenFile}")
-        logger.info("runNodeUpdater: pnpm=${extension.pnpmEnable}, requireHomeNodeExec=${extension.requireHomeNodeExec}")
-
-        val flowResourcesFolder = File(extension.npmFolder, DEAULT_FLOW_RESOURCES_FOLDER)
-        // @formatter:off
-        extension.createNodeTasksBuilder(project)
-                .runNpmInstall(extension.runNpmInstall)
-                .useV14Bootstrap(extension.useDeprecatedV14Bootstrapping)
-                .enablePackagesUpdate(true)
-                .useByteCodeScanner(extension.optimizeBundle)
-                .withFlowResourcesFolder(flowResourcesFolder)
-                .copyResources(jarFiles)
-                .copyLocalResources(extension.frontendResourcesDirectory)
-                .enableImportsUpdate(true)
-                .withEmbeddableWebComponents(extension.generateEmbeddableWebComponents)
-                .withTokenFile(tokenFile)
-                .enablePnpm(extension.pnpmEnable)
-                .withConnectApplicationProperties(extension.applicationProperties)
-                .withConnectJavaSourceFolder(extension.javaSourceFolder)
-                .withConnectGeneratedOpenApiJson(extension.openApiJsonFile)
-                .withConnectClientTsApiFolder(extension.generatedTsFolder)
-                .build().execute()
-
-        logger.info("runNodeUpdater: done!")
-    }
-
-    /**
-     * Add the devMode token to build token file so we don't try to start the
-     * dev server. Remove the abstract folder paths as they should not be used
-     * for prebuilt bundles.
-     */
-    private fun updateBuildFile(tokenFile: File) {
-        check(tokenFile.isFile) { "$tokenFile is missing" }
-
-        val buildInfo: JsonObject = JsonUtil.parse(tokenFile.readText())
-        buildInfo.apply {
-            remove(Constants.NPM_TOKEN)
-            remove(Constants.GENERATED_TOKEN)
-            remove(Constants.FRONTEND_TOKEN)
-            remove(InitParameters.SERVLET_PARAMETER_ENABLE_PNPM)
-            remove(InitParameters.REQUIRE_HOME_NODE_EXECUTABLE)
-            remove(InitParameters.SERVLET_PARAMETER_DEVMODE_OPTIMIZE_BUNDLE);
-            remove(Constants.CONNECT_JAVA_SOURCE_FOLDER_TOKEN);
-            remove(Constants.CONNECT_APPLICATION_PROPERTIES_TOKEN);
-            remove(Constants.CONNECT_JAVA_SOURCE_FOLDER_TOKEN);
-            remove(Constants.CONNECT_OPEN_API_FILE_TOKEN);
-            remove(Constants.CONNECT_GENERATED_TS_DIR_TOKEN);
-
-            put(InitParameters.SERVLET_PARAMETER_ENABLE_DEV_SERVER, false)
-        }
-        buildInfo.writeToFile(tokenFile)
-        logger.info("Updated token file $tokenFile to $buildInfo")
     }
 }
--- src/main/kotlin/com/vaadin/gradle/VaadinPrepareFrontendTask.kt
+++ src/main/kotlin/com/vaadin/gradle/VaadinPrepareFrontendTask.kt
@@ -15,20 +15,11 @@
  */
 package com.vaadin.gradle
 
-import com.vaadin.flow.server.Constants
-import com.vaadin.flow.server.InitParameters
-import com.vaadin.flow.server.frontend.FrontendTools
-import com.vaadin.flow.server.frontend.FrontendUtils
-import com.vaadin.flow.server.frontend.NodeTasks
-import elemental.json.Json
-import elemental.json.JsonObject
+import com.vaadin.flow.plugin.base.BuildFrontendUtil
 import org.gradle.api.DefaultTask
 import org.gradle.api.Task
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.bundling.War
-import java.io.File
-import java.nio.file.Files
 
 /**
  * This task checks that node and npm tools are installed, copies frontend
@@ -62,77 +53,6 @@ public open class VaadinPrepareFrontendTask : DefaultTask() {
     public fun vaadinPrepareFrontend() {
         val extension: VaadinFlowPluginExtension = VaadinFlowPluginExtension.get(project)
         logger.info("Running the vaadinPrepareFrontend task with effective configuration $extension")
-
-        Files.createDirectories(extension.frontendDirectory.toPath())
-        Files.createDirectories(extension.buildOutputDirectory.toPath())
-        Files.createDirectories(extension.webpackOutputDirectory!!.toPath())
-
-        propagateBuildInfo(extension)
-
-        val tools: FrontendTools = extension.createFrontendTools()
-        if (extension.requireHomeNodeExec) {
-            tools.forceAlternativeNodeExecutable()
-        }
-        logger.info("validating node and npm version")
-        tools.validateNodeAndNpmVersion()
-
-        // produce target/frontend/package.json
-        Files.createDirectories(extension.generatedFolder.toPath())
-
-        logger.info("Running NodeTasks")
-        logger.info("runNodeUpdater: npmFolder=${extension.npmFolder}, generatedPath=${extension.generatedFolder}, frontendDirectory=${extension.frontendDirectory}")
-        logger.info("runNodeUpdater: withWebpack(${extension.webpackOutputDirectory}, ${extension.webpackTemplate}, ${extension.webpackGeneratedTemplate})")
-        logger.info("runNodeUpdater: createMissingPackageJson(true), enableImportsUpdate(false), enablePackagesUpdate(false)")
-        logger.info("runNodeUpdater: not running npm install: it's supposed to be run either by Vaadin Servlet in development mode, or by the `vaadinBuildFrontend` task.")
-
-        val flowResourcesFolder = File(extension.npmFolder, FrontendUtils.DEAULT_FLOW_RESOURCES_FOLDER)
-        val builder: NodeTasks.Builder = extension.createNodeTasksBuilder(project)
-                .withWebpack(extension.webpackOutputDirectory!!, extension.webpackTemplate, extension.webpackGeneratedTemplate)
-                .useV14Bootstrap(extension.useDeprecatedV14Bootstrapping)
-                .withFlowResourcesFolder(flowResourcesFolder)
-                .createMissingPackageJson(true)
-                .enableImportsUpdate(false)
-                .enablePackagesUpdate(false)
-                .runNpmInstall(false)
-
-        // If building a jar project copy jar artifact contents now as we might
-        // not be able to read files from jar path.
-        val isJarPackaging: Boolean = project.tasks.withType(War::class.java).isEmpty()
-        if (isJarPackaging) {
-            val jarFiles: Set<File> = project.configurations
-                    .getByName("runtimeClasspath")
-                    .resolve()
-                    .filter { it.name.endsWith(".jar", true) }
-                    .toSet()
-            logger.info("runNodeUpdater: using JAR packaging, copyResources=${jarFiles.toPrettyFormat()}")
-            builder.copyResources(jarFiles)
-        } else {
-            logger.info("Not copying resources since we're not using JAR packaging")
-        }
-
-        builder.build().execute()
-        logger.info("runNodeUpdater: done!")
-    }
-
-    private fun propagateBuildInfo(extension: VaadinFlowPluginExtension) {
-        val configFolder = File("${extension.buildOutputDirectory}/META-INF/VAADIN/config")
-        Files.createDirectories(configFolder.toPath())
-        val buildInfo: JsonObject = Json.createObject().apply {
-            put(InitParameters.SERVLET_PARAMETER_PRODUCTION_MODE, extension.productionMode)
-            put(InitParameters.SERVLET_PARAMETER_USE_V14_BOOTSTRAP, extension.useDeprecatedV14Bootstrapping);
-            put(InitParameters.SERVLET_PARAMETER_INITIAL_UIDL, extension.eagerServerLoad);
-            put(Constants.NPM_TOKEN, extension.npmFolder.absolutePath)
-            put(Constants.GENERATED_TOKEN, extension.generatedFolder.absolutePath)
-            put(Constants.FRONTEND_TOKEN, extension.frontendDirectory.absolutePath)
-            put(Constants.CONNECT_JAVA_SOURCE_FOLDER_TOKEN, extension.javaSourceFolder.absolutePath);
-            put(Constants.CONNECT_APPLICATION_PROPERTIES_TOKEN, extension.applicationProperties.absolutePath);
-            put(Constants.CONNECT_OPEN_API_FILE_TOKEN, extension.openApiJsonFile.absolutePath);
-            put(Constants.CONNECT_GENERATED_TS_DIR_TOKEN, extension.generatedTsFolder.absolutePath);
-            put(InitParameters.SERVLET_PARAMETER_ENABLE_PNPM, extension.pnpmEnable)
-            put(InitParameters.REQUIRE_HOME_NODE_EXECUTABLE, extension.requireHomeNodeExec)
-        }
-        val tokenFile = File(configFolder, "flow-build-info.json")
-        buildInfo.writeToFile(tokenFile)
-        logger.info("Wrote token file $tokenFile: $buildInfo")
+        BuildFrontendUtil.prepareFrontend(GradlePluginAdapter(project))
     }
 }
