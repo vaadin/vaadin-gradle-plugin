efe01f8a82db9336d60a3611a0524ac6e8fde374
Martin Vysny <mavi@vaadin.com>
2021-11-30 19:29:15 +0200

    vaadin v bump

--- src/functionalTest/kotlin/com/vaadin/gradle/AllTests.kt
+++ src/functionalTest/kotlin/com/vaadin/gradle/AllTests.kt
@@ -8,7 +8,7 @@ import kotlin.properties.ReadWriteProperty
 /**
  * The Vaadin version to test with. Generally the newest 14.x release.
  */
-val vaadin14Version = "14.7.5"
+val vaadin14Version = "14.7.7"
 
 /**
  * Sets up a folder for a test project -
